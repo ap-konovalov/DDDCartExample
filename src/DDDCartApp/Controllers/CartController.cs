d508a551d4b2022a398943ec7914637f49f40c19
d.afonchenko <d.afonchenko@dodopizza.com>
2020-05-09 23:21:15 +0300

    Intial commit

---
+++ src/DDDCartApp/Controllers/CartController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DDDCartApp.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class CartController : ControllerBase
+	{
+		private readonly ILogger<CartController> _logger;
+
+		public CartController(ILogger<CartController> logger)
+		{
+			_logger = logger;
+		}
+	}
+}
